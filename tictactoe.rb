79aac2d5234f29f37007605e8425c3a5488b4058
Tarik Merrlees <tarik.merrylees@madetech.com>
2022-02-14 13:57:27 +0000

    Passed fourth test, game over if three crosses in top row

--- tictactoe.rb
+++ tictactoe.rb
@@ -10,7 +10,12 @@ class Tictactoe
 
   def player_adds_cross(selected_square)
     @rows[selected_square - 1] = "X"
-    @rows
+    if @rows[0] == @rows[1] && @rows[1] == @rows[2]
+      puts "Game over, Player 1 wins!"
+      return "Game over, Player 1 wins!"
+    else
+      @rows
+    end
   end
 
   def player_adds_naught(selected_square)
