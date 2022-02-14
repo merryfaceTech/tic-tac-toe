5210aa642c91d32da9bc3a980a890d28c33ff403
Tarik Merrlees <tarik.merrylees@madetech.com>
2022-02-14 14:53:57 +0000

    partially refactored

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -7,17 +7,29 @@ describe Tictactoe do
 
   it "start new game with an empty board in the beginning of the game" do
     result = @tictactoe.start_new_game
-    expect(result).to eq(["", "", "", "", "", "", "", "", ""])
+    expect(result).to eq([
+      ["", "", ""],
+      ["", "", ""],
+      ["", "", ""]
+    ])
   end
 
   it "starts a new game and player adds X to top left corner" do
     result = @tictactoe.player_adds_cross(1)
-    expect(result).to eq(["X", "", "", "", "", "", "", "", ""])
+    expect(result).to eq([
+      ["X", "", ""],
+      ["", "", ""],
+      ["", "", ""]
+    ])
   end
 
   it "starts a new game and player adds naught to top left corner" do
     result = @tictactoe.player_adds_naught(1)
-    expect(result).to eq(["O", "", "", "", "", "", "", "", ""])
+    expect(result).to eq([
+      ["O", "", ""],
+      ["", "", ""],
+      ["", "", ""]
+    ])
   end
 
   it "Player wins by drawing 3 crosses in top row" do
--- tictactoe.rb
+++ tictactoe.rb
@@ -1,17 +1,39 @@
 class Tictactoe
   def initialize
-    @rows = ["", "", "", "", "", "", "", "", ""]
+    @row1=["", "", ""]
+    @row2=["", "", ""]
+    @row3=["", "", ""]
+    @rows = [
+      @row1,
+      @row2,
+      @row3
+    ]
   end
 
   def start_new_game
-    @rows = ["", "", "", "", "", "", "", "", ""]
+    @rows = [
+      ["", "", ""],
+      ["", "", ""],
+      ["", "", ""]
+    ]
     @rows
   end
 
   def player_adds_cross(selected_square)
-    @rows[selected_square - 1] = "X"
-    if @rows[0] == @rows[1] && @rows[1] == @rows[2]
-      puts "Game over, Player 1 wins!"
+    if selected_square <= 3
+      @row1[selected_square - 1] = "X"
+      @rows[0] = @row1
+      puts @row1
+    elsif selected_square >= 3 && selected_square <= 6
+      @row2[selected_square - 1] = "X"
+      @rows[1] = @row2
+    else
+      @row3[selected_square - 1] = "X"
+      @rows[2] = @row3
+    end
+
+    if @row1[0] == @row1[1] && @row1[1] == @row1[2]
+      # puts "Game over, Player 1 wins!"
       return "Game over, Player 1 wins!"
     else
       @rows
