7489b2ce02902f08d4ced716a3c2f19584b7b92c
Tarik Merrlees <tarik.merrylees@madetech.com>
2022-02-14 13:39:37 +0000

    Added second test, puts X top left corner

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -9,4 +9,10 @@ describe Tictactoe do
     result = @tictactoe.start_new_game
     expect(result).to eq(["", "", "", "", "", "", "", "", ""])
   end
+
+  it "starts a new game and player adds X to top left corner" do
+    result = @tictactoe.player_adds_cross
+    expect(result).to eq(["X", "", "", "", "", "", "", "", ""])
+  end
+
 end
