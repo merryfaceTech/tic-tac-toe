5a00e4138d6a4b3f8f1731efbb80ca0392b8fda5
YasminMorshed <snowmoss10@gmail.com>
2022-02-16 08:55:42 +0000

    Added second test to get user input and update the top left corner, displaying grid, fails

--- spec/game_screen_spec.rb
+++ spec/game_screen_spec.rb
@@ -1,4 +1,4 @@
-require_relative "../utils/Game_screen"
+require_relative '../utils/Game_screen'
 
 describe Game_screen do
   game = described_class.new
@@ -6,12 +6,18 @@ describe Game_screen do
   # Test 1
   it "displays an empty grid at round 0" do
     empty_grid = "   |   |   \n-----------\n   |   |   \n-----------\n   |   |   "
-    puts empty_grid
     grid = game.initiateGame
     expect(grid).to eq(empty_grid)
   end
-end
 
+  # Test 2
+  it "gets player input to update top left box, displays updated grid" do
+    top_left_corner = " X |   |   \n-----------\n   |   |   \n-----------\n   |   |   "
+    puts top_left_corner
+    grid_r1 = game.round1
+    expect(grid_r1).to eq(top_left_corner)  
+  end
+end
 # empty "   "
 # squares = ["   ", "   ", "   "]
 
--- utils/game_screen.rb
+++ utils/game_screen.rb
@@ -1,10 +1,18 @@
+require_relative '../utils/Tictactoe'
+
 class Game_screen
     def initiateGame
-        "   |   |   \n-----------   |   |   \n-----------   |   |   "
+        empty_grid = "   |   |   \n-----------\n   |   |   \n-----------\n   |   |   "
+        puts empty_grid
+        empty_grid
+    end
+
+    def round1
+        puts "Hey, welcome to TIC-TAC-TOE. Please enter the square you would like to use!"
+        choice1 = gets.chomp
+        if choice1 == 1
+            @row1[0] = "X"
+            grid_r1 = " @row1[0]  |   |   \n-----------   |   |   \n-----------   |   |   "
+        end
     end
 end
-
-    
-    puts "Enter your name: "
-    name = gets.chomp
-    puts "Hello #{name}, how are you"
