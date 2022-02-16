b4cc32239a469ddc9a9039015bc65efb2eb8e3a1
Tarik Merrlees <tarik.merrylees@madetech.com>
2022-02-16 17:33:19 +0000

    WIP: refactoring tests to check block output instead of returned values

--- spec/game_screen_spec.rb
+++ spec/game_screen_spec.rb
@@ -7,23 +7,29 @@ describe Game_screen do
 
   # Test 1
   it "displays an empty grid at round 0" do
-    empty_grid = "   |   |   \n-----------\n   |   |   \n-----------\n   |   |   "
+    empty_grid = "
+       |   |   
+    ------------
+       |   |   
+    ------------
+       |   |    
+    "
     grid = game_ui.initiateGame
-    expect(grid).to eq(empty_grid)
+    expect{ grid }.to output(empty_grid).to_stdout
   end
 
   # Test 2
   it "gets player input to update top left box, displays updated grid" do
     top_left_corner = " X |   |   \n-----------\n   |   |   \n-----------\n   |   |   "
     grid = game_ui.player_chooses_square
-    expect(grid).to eq(top_left_corner)
+    expect{ grid }.to output(top_left_corner).to_stdout
   end
 
   # Test 3
   it "AI makes first choice, displays updated grid" do
     middle_box = " X |   |   \n-----------\n   | O |   \n-----------\n   |   |   "
     grid = game_ui.ai_chooses_square
-    expect(grid).to eq(middle_box)
+    expect{ grid }.to output(middle_box).to_stdout
   end
 
   # Test 4
@@ -32,6 +38,6 @@ describe Game_screen do
     player_win_message = "Game over, Player wins!"
     game_ui.player_chooses_square
     grid = game_ui.player_chooses_square
-    expect(grid).to eq(winning_grid + "\n \n " + player_win_message)
+    expect{ grid }.to output(winning_grid + "\n \n " + player_win_message).to_stdout
   end
 end
--- utils/game_screen.rb
+++ utils/game_screen.rb
@@ -8,34 +8,44 @@ class Game_screen
         @instructional_grid = " 1 | 2 | 3 \n-----------\n 4 | 5 | 6 \n-----------\n 7 | 8 | 9 \n \n"
     end
 
-    def update_grid(show_current_grid)
+    def update_grid
         @grid = " #{@boxes[0][0]} | #{@boxes[0][1]} | #{@boxes[0][2]} \n-----------\n #{@boxes[1][0]} | #{@boxes[1][1]} | #{@boxes[1][2]} \n-----------\n #{@boxes[2][0]} | #{@boxes[2][1]} | #{@boxes[2][2]} "
-        if show_current_grid
-            puts "\n \n" + @grid
-        end
-        @grid
+    end
+
+    def print_grid
+        puts "\n \n" + @grid
     end
 
     def initiateGame
         puts "Hey, welcome to TIC-TAC-TOE.\n \n"
         puts @instructional_grid
         @boxes = @TicTactoe.start_new_game
-        update_grid(false)
+        update_grid()
+        @grid
     end
 
     def player_chooses_square
         puts "Please enter the square you would like to use! \n \n"
         choice = gets.chomp.to_i
         @TicTactoe.player_adds_cross(choice)
-        end_game_message = @TicTactoe.game_end_checker(true).is_a?(String) ? "\n \n " + @TicTactoe.game_end_checker(true) : ''
-        @boxes = @TicTactoe.get_rows
-        update_grid(true) + end_game_message
+        end_game_message = "\n \n " + @TicTactoe.game_end_checker(true)
+        game_has_ended = @TicTactoe.game_end_checker(true).is_a?(String)
+        @boxes = @TicTactoe.get_rows     
+        update_grid()
+        print_grid()
+
+        if game_has_ended
+            puts end_game_message
+        end
+        
     end
 
     def ai_chooses_square
         @TicTactoe.ai_adds_naught(5)
         @boxes = @TicTactoe.get_rows
-        update_grid(true)
+        update_grid()
+        print_grid()
+        @grid
     end
 
 end
