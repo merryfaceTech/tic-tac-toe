2006e13c48e909024d3690d89a9ea65be2db7fd0
YasminMorshed <snowmoss10@gmail.com>
2022-02-18 16:03:16 +0000

    Added lambda function, changed test 1, 3 tests fail

--- spec/game_screen_spec.rb
+++ spec/game_screen_spec.rb
@@ -2,20 +2,15 @@ require_relative '../utils/Game_screen'
 require_relative '../utils/TicTactoe'
 
 describe Game_screen do
-  game_ui = described_class.new
+  game_ui = Game_screen.new
   game_logic = Tictactoe.new
 
   # Test 1
-  it "displays an empty grid at round 0" do
-    empty_grid = "
-       |   |   
-    ------------
-       |   |   
-    ------------
-       |   |    
-    "
-    grid = game_ui.initiateGame
-    expect{ grid }.to output(empty_grid).to_stdout
+  it "displays message and grid with numbers at round 0" do
+    empty_grid = "Hey, welcome to TIC-TAC-TOE.\n \n 1 | 2 | 3 \n-----------\n 4 | 5 | 6 \n-----------\n 7 | 8 | 9 \n \n"
+    # grid = game_ui.initiateGame
+    grid = lambda { game_ui.initiateGame }
+    expect{ grid.call }.to output(empty_grid).to_stdout
   end
 
   # Test 2
