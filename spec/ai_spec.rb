f6dcaaf15c255d6fdb52e33ff64d9650cfdd1818
Tarik Merrlees <tarik.merrylees@madetech.com>
2022-03-07 14:19:06 +0000

    Added second test, AI stops player from winning, fails

--- spec/ai_spec.rb
+++ spec/ai_spec.rb
@@ -2,17 +2,41 @@
 require './utils/ai'
 
 describe Ai do
-  # Test 1
-  it "Retrieves all available squares" do
-    ai = Ai.new
-    testing_grid = [
-      ["X", "X", "O"],
-      [" ", "O", "O"],
-      [" ", " ", "X"],
-    ]
-    free_squares = [4, 7, 8]
-    result = ai.get_free_squares(testing_grid)
+    # Test 1
+    it "Retrieves all available squares" do
+      ai = Ai.new
 
-    expect(result).to eq(free_squares)
-  end
+      testing_grid = [
+        ["X", "X", "O"],
+        [" ", "O", "O"],
+        [" ", " ", "X"],
+      ]
+
+      free_squares = [4, 7, 8]
+
+      result = ai.get_free_squares(testing_grid)
+
+      expect(result).to eq(free_squares)
+    end
+
+    # Test 2
+    it "Stops player from winning by blocking its path on first row" do
+        ai = Ai.new
+
+        testing_grid = [
+          ["X", "X", " "],
+          [" ", "O", " "],
+          [" ", " ", " "],
+        ]
+
+        result = ai.ai_chooses_box(testing_grid)
+
+        stop_win_grid = [
+            ["X", "X", "O"],
+            [" ", "O", " "],
+            [" ", " ", " "],
+          ]
+    
+        expect(result).to eq(stop_win_grid)
+      end
 end
--- utils/ai.rb
+++ utils/ai.rb
@@ -1,23 +1,21 @@
 require_relative './Tictactoe'
 
 class Ai
+    def get_free_squares(grid)
 
-  def get_free_squares(grid)
+      free_squares = []
 
-    free_squares = []
+      grid.each_with_index do |row, row_index|
 
-    grid.each_with_index do |row, row_index|
+          row.each_with_index do |column, column_index|
+              if column == " "
+                  free_squares.append(column_index + 1) if row_index == 0
+                  free_squares.append(column_index + 4) if row_index == 1
+                  free_squares.append(column_index + 7) if row_index == 2
+              end
+          end
+      end
 
-        row.each_with_index do |column, column_index|
-            if column == " "
-                free_squares.append(column_index + 1) if row_index == 0
-                free_squares.append(column_index + 4) if row_index == 1
-                free_squares.append(column_index + 7) if row_index == 2
-            end
-        end
+      return free_squares
     end
-
-    return free_squares
-  end
-
 end
