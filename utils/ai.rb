74f300cebab5a6bea8657ec4186adc5fe955d6ec
Tarik Merrlees <tarik.merrylees@madetech.com>
2022-03-07 14:13:43 +0000

    Passes first test in AI, get free squares

--- utils/ai.rb
+++ utils/ai.rb
@@ -1,7 +1,23 @@
 require_relative './Tictactoe'
 
 class Ai
-  def execute
-    'something'
+
+  def get_free_squares(grid)
+
+    free_squares = []
+
+    grid.each_with_index do |row, row_index|
+
+        row.each_with_index do |column, column_index|
+            if column == " "
+                free_squares.append(column_index + 1) if row_index == 0
+                free_squares.append(column_index + 4) if row_index == 1
+                free_squares.append(column_index + 7) if row_index == 2
+            end
+        end
+    end
+
+    return free_squares
   end
+
 end
