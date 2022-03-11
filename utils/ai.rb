5a39b01929416c8ed1d0f1324de8dbda9cb3b725
Tarik Merrlees <tarik.merrylees@madetech.com>
2022-03-11 18:32:41 +0000

    wip

--- utils/ai.rb
+++ utils/ai.rb
@@ -7,9 +7,9 @@ class Ai
     grid.each_with_index do |row, row_index|
       row.each_with_index do |column, column_index|
         if column == " "
-          free_squares.append(column_index + 1) if row_index == 0
-          free_squares.append(column_index + 4) if row_index == 1
-          free_squares.append(column_index + 7) if row_index == 2
+          free_squares << column_index + 1 if row_index == 0
+          free_squares << column_index + 4 if row_index == 1
+          free_squares << column_index + 7 if row_index == 2
         end
       end
     end
@@ -18,8 +18,8 @@ class Ai
   end
 
   def ai_chooses_box(grid)
-    minimax(grid)[1]
-    # get_free_squares(grid).sample(1)[0]
+    # minimax(grid)[1]
+    get_free_squares(grid).sample(1)[0]
   end
 
   def check_who_won(grid, symbol)
@@ -69,17 +69,19 @@ class Ai
     return -1 if check_who_won(grid, "X") == :lose
     return 0 if check_who_won(grid, "O") == :draw
 
-
     get_free_squares(grid).each do |square|
       possible_game = set_box(grid, square, current_player_is_ai ? "O" : "X")
-      score = minimax(possible_game, scores, !current_player_is_ai)
+      score = -1
 
-      if !score.nil?
-        scores << [score[0], square]
-      else
-        scores << [-1, square]
+      if minimax(possible_game, scores, !current_player_is_ai) != nil
+        score = minimax(possible_game, scores, !current_player_is_ai)[0]
       end
+
+      scores << [score, square]
+      break if score == 1
     end
+
+    pp scores
 
     return scores.max if current_player_is_ai
     return scores.min if !current_player_is_ai
